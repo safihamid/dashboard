844b9135f4e49cde0e7e31ef05ee88db4e4ca375
cpirich <chris@code.org>
2014-01-09 15:14:01 -0800

    remove dash1 from deploy, make dash5 :db and :primary

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
 set :rails_env, 'production'
-server 'dash1.code.org', :app, :web, :db, :primary => true
-server 'dash5.code.org', :app, :web
+server 'dash5.code.org', :app, :web, :db, :primary => true
 server 'dash6.code.org', :app, :web
 server 'dash7.code.org', :app, :web
 server 'dash8.code.org', :app, :web
