530802c507e78d1c420a287cc3e324778336ca0d
Tobias Kahan <tobyk100@gmail.com>
2014-01-21 15:24:25 -0800

    Update prod machines.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,3 @@ server 'dash6.code.org', :app, :web
 server 'dash7.code.org', :app, :web
 server 'dash8.code.org', :app, :web
 server 'dash9.code.org', :app, :web
-server 'dash13.code.org', :app, :web
