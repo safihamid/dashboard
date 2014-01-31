7a272f6ed9f24b87755de2d4a3ccdb49dbcfd9b1
Tobias Kahan <tobyk100@gmail.com>
2014-01-31 14:41:19 -0800

    Remove terminated machines from deploy.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,3 @@ set :rails_env, 'production'
 server 'dash5.code.org', :app, :web, :db, :primary => true
 server 'dash6.code.org', :app, :web
 server 'dash7.code.org', :app, :web
-server 'dash8.code.org', :app, :web
-server 'dash9.code.org', :app, :web
