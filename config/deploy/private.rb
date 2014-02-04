393355d9915825edcd9bf2d12a3a356d1a02d2de
Tobias Kahan <tobyk100@gmail.com>
2014-02-04 15:27:02 -0800

    Update private group.

--- config/deploy/private.rb
+++ config/deploy/private.rb
@@ -1,2 +1,2 @@
 set :rails_env, 'production'
-server 'ec2-54-80-1-183.compute-1.amazonaws.com', :app, :web, :db, :primary => true
+server 'ec2-50-17-122-132.compute-1.amazonaws.com', :app, :web, :db, :primary => true
