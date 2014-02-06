c9a1d8d6292d2faae2ea9ef7808d9ca7d9a9d05c
Nan <nan@code.org>
2014-02-06 13:14:19 -0800

    Only admin can access this page.

--- app/controllers/frequent_unsuccessful_level_sources_controller.rb
+++ app/controllers/frequent_unsuccessful_level_sources_controller.rb
@@ -1,5 +1,7 @@
 class FrequentUnsuccessfulLevelSourcesController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
+    raise "unauthorized" if !current_user.admin?
   end
 end
